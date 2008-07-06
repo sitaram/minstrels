47d5a90c57bde03c5362576a70b7a72936dc5cf9
sitaram <sitaram@MV-144816222.corp.google.com>
2008-07-06 15:22:23 -0700

    guestbook with templates and static dir

--- minstrels.py
+++ minstrels.py
@@ -1,6 +1,60 @@
-from google.appengine.api import users
+import cgi
+import os
+import wsgiref.handlers
 
-print "Content-Type: text/html"
-print ""
-print "Foo"
-print users.get_current_user()
+from google.appengine.api import users
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+
+class Greeting(db.Model):
+  author = db.UserProperty()
+  content = db.StringProperty(multiline=True)
+  date = db.DateTimeProperty(auto_now_add=True)
+
+
+class Guestbook(webapp.RequestHandler):
+  def post(self):
+    greeting = Greeting()
+
+    if users.get_current_user():
+      greeting.author = users.get_current_user()
+
+    greeting.content = self.request.get('content')
+    greeting.put()
+    self.redirect('/')
+
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    greetings_query = Greeting.all().order('-date')
+    greetings = greetings_query.fetch(10)
+
+    if users.get_current_user():
+      url = users.create_logout_url(self.request.uri)
+      url_linktext = 'Logout'
+    else:
+      url = users.create_login_url(self.request.uri)
+      url_linktext = 'Login'
+
+    template_values = {
+      'greetings': greetings,
+      'url': url,
+      'url_linktext': url_linktext,
+      }
+
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, template_values))
+
+
+def main():
+  application = webapp.WSGIApplication(
+                                       [('/', MainPage),
+                                        ('/sign', Guestbook)],
+                                       debug=True)
+  wsgiref.handlers.CGIHandler().run(application)
+
+
+if __name__ == "__main__":
+  main()
